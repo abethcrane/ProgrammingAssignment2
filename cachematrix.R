cc6161ca4bd79db3a337d64b404cd30ab4d3e832
beth <abethcrane@gmail.com>
2015-12-26 16:07:30 -0800

    Updated to fix spaces/tabs, and add in error handling for non-square invertible matrices

--- cachematrix.R
+++ cachematrix.R
@@ -1,51 +1,58 @@
 # makeCacheMatrix returns an object that stores both a matrix and its inverse
 # This saves time for operations that involve frequent lookups of inverse matrices
 makeCacheMatrix <- function(m = matrix()) {
-  inverse <- NULL
-  isCached <- FALSE
-  
-  # Initialize our matrix to the parameter, and our inverse to NULL
-  set <- function(x) {
-    m <<- x
-    inverse <<- NULL
-    isCached <<- FALSE
-  }
-  
-  # Return the matrix
-  get <- function() m
-  
-  # Set the inverse matrix to the parameter
-  setInverse <- function(i) {
-    inverse <<- i
-    isCached <<- TRUE
-  }
-  
-  # Return our inverse matrix
-  getInverse <- function() inverse
-  
-  # Returns whether or not the inverse has been cached
-  getIsCached <- function() isCached
-  
-  # List out the functions of makeCacheMatrix
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse,
-       getIsCached = getIsCached)
+    inverse <- NULL
+    isCached <- FALSE
+    
+    # Initialize our matrix to the parameter, and our inverse to NULL
+    set <- function(x) {
+        m <<- x
+        inverse <<- NULL
+        isCached <<- FALSE
+    }
+    
+    # Return the matrix
+    get <- function() m
+    
+    # Set the inverse matrix to the parameter
+    setInverse <- function(i) {
+        inverse <<- i
+        isCached <<- TRUE
+    }
+    
+    # Return our inverse matrix
+    getInverse <- function() inverse
+    
+    # Returns whether or not the inverse has been cached
+    getIsCached <- function() isCached
+    
+    # List out the functions of makeCacheMatrix
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse,
+        getIsCached = getIsCached)
 }
 
 # cacheSolve takes a cacheMatrix and returns the inverse
 # the inverse is either pre-cached, or solved and then cached for next time
 cacheSolve <- function(m, ...) {
-  # Check if the inverse has been cached
-  if (m$getIsCached()) {
-    inverse <- m$getInverse()
+    # Set matrix to NULL - this will be returned in the case of an error
+    inverse <- NULL
+    
+    # Check if the inverse has been cached
+    if (m$getIsCached()) {
+        inverse <- m$getInverse()
     # If it hasn't been, calculate it and cache it
-  } else {
-    data <- m$get() # retrieve the matrix from our cacheMatrix parameter
-    inverse <- solve(data)
-    m$setInverse(inverse)
-  }
+    } else {
+        data <- m$get() # retrieve the matrix from our cacheMatrix parameter
+        tryCatch({
+            inverse <- solve(data)
+            m$setInverse(inverse)
+        }, error = function(e) {
+            print("Unfortunately only square matrices can be inverted")
+        })
+    }
   
-  # Return the inverse
-  inverse
+    # Return the inverse (or NULL in case of)
+    inverse
 }
